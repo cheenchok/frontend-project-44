4a0caef03b121d4f62eccb7955534969bbf6e846
cheenchok <cheenchok@gmail.com>
2024-06-01 14:53:55 +0300

    feat: move brain-even game to func

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,47 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import { getAnswer, getRandomIntInclusive } from '../src/index.js';
+import runBrainEvenGame from '../src/games/brain-even.js';
 
-function getName() {
-  return readlineSync.question('May I have your name? ');
-}
-
-const evenNumber = (number) => {
-  if (number % 2 === 0) {
-    return 'yes';
-  }
-  return 'no';
-};
-
-console.log('Welcome to the Brain Games!');
-
-const name = getName();
-
-console.log(`Hello, ${name}!`);
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-let correctAnswers = 0;
-
-for (let i = 0; i < 3; i += 1) {
-  const randomNumber = getRandomIntInclusive(1, 100);
-
-  console.log(`Question: ${randomNumber}`);
-
-  const myAnswer = getAnswer();
-  const computerAnswer = evenNumber(randomNumber);
-
-  if (myAnswer === computerAnswer) {
-    correctAnswers += 1;
-    console.log('Correct!');
-  } else {
-    console.log(
-      `'${myAnswer}' is wrong answer ;(. Correct answer was '${computerAnswer}'`,
-    );
-    console.log(`Let's try again, ${name}!`);
-    break;
-  }
-}
-
-if (correctAnswers === 3) {
-  console.log(`Congratulations, ${name}!`);
-}
+runBrainEvenGame();
---
+++ src/games/brain-even.js
@@ -0,0 +1,45 @@
+import readlineSync from 'readline-sync';
+import { getAnswer, getRandomIntInclusive } from '../index.js';
+
+function greeting() {
+  console.log('Welcome to the Brain Games!');
+}
+
+function getName() {
+  return readlineSync.question('May I have your name? ');
+}
+
+const evenNumber = (number) => {
+  if (number % 2 === 0) {
+    return 'yes';
+  }
+  return 'no';
+};
+
+export default function runBrainEvenGame() {
+  greeting();
+
+  const name = getName();
+
+  console.log(`Hello, ${name}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  for (let i = 0; i < 3; i += 1) {
+    const randomNumber = getRandomIntInclusive(1, 100);
+
+    console.log(`Question: ${randomNumber}`);
+    const myAnswer = getAnswer();
+    const computerAnswer = evenNumber(randomNumber);
+
+    if (myAnswer !== computerAnswer) {
+      console.log(
+        `'${myAnswer}' is wrong answer ;(. Correct answer was '${computerAnswer}'`,
+      );
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+    console.log('Correct!');
+  }
+
+  console.log(`Congratulations, ${name}!`);
+}
